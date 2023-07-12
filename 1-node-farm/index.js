a5053ca444bded6261324c73a423042c15ce2b6a
AgentKazy <kazygaming@outlook.pt>
2023-07-12 12:30:23 +0100

    009. Blocking and Non-Blocking: Async Node

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,7 +1,14 @@
 const fs = require('fs');
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// Blocking code execution
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 // console.log(textIn);
+
+// Non-blocking code execution
+fs.readFile('./txt/input.txt', 'utf-8', (err, data) => {
+  console.log(data);
+});
+console.log('Reading file...');
 
 const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
 fs.writeFileSync('./txt/output.txt', textOut);
