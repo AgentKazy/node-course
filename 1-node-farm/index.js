423b7a826f9c687ff57f4a4f9ec8c33077ad4661
AgentKazy <kazygaming@outlook.pt>
2023-07-13 12:09:43 +0100

    012. Routing

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,6 +1,8 @@
-// Modules
+/////////////////////////////////////////////////////////////
+// MODULES
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 /////////////////////////////////////////////////////////////
 // FILES
@@ -33,7 +35,19 @@ console.log('File saved!'); */
 // SERVER
 // req - request, res - response
 const server = http.createServer((req, res) => {
-  res.end('Hello from the server!');
+  const pathName = req.url;
+
+  if (pathName === '/' || pathName === '/overview') {
+    res.end('This is the OVERVIEW');
+  } else if (pathName === '/product') {
+    res.end('This is the PRODUCT');
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/HTML',
+      'my-own-header': 'hello-world',
+    });
+    res.end('<h1>Page not found!</h1>');
+  }
 });
 
 const port = 8000;
