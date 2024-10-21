730ee8f6e9213ff1c1f5dba7555ac05d5365eeeb
AgentKazy <kazygaming@outlook.pt>
2024-10-21 14:09:48 +0100

    Step 011

--- section-2-intro/1-node-farm/index.js
+++ section-2-intro/1-node-farm/index.js
@@ -1,5 +1,8 @@
 const fs = require('node:fs');
+const http = require('node:http');
 
+//////////////////////
+// FILES
 // Blocking, synchronous
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 // console.log(textIn);
@@ -8,18 +11,29 @@ const fs = require('node:fs');
 // console.log('File written.');
 
 // Non-blocking, asynchronous
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-  if (err) return console.log('Error!');
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//   if (err) return console.log('Error!');
 
-  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-    console.log(data2);
-    fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
-      console.log(data3);
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2);
+//     fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//       console.log(data3);
 
-      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
-        console.log('File has been written.');
-      });
-    });
-  });
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
+//         console.log('File has been written.');
+//       });
+//     });
+//   });
+// });
+// console.log('Will read file.');
+
+//////////////////////
+// SERVER
+
+const server = http.createServer((req, res) => {
+  res.end('Hello from the server.');
 });
-console.log('Will read file.');
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000.');
+});
