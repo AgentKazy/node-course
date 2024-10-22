015f5017e0494c09a5614db8d2634919f7c7c33d
AgentKazy <kazygaming@outlook.pt>
2024-10-22 23:06:20 +0100

    Step 020

--- section-2-intro/1-node-farm/index.js
+++ section-2-intro/1-node-farm/index.js
@@ -2,6 +2,8 @@ const fs = require('node:fs');
 const http = require('node:http');
 const url = require('node:url');
 
+const slugify = require('slugify');
+
 const replaceTemplate = require('./modules/replaceTemplate');
 
 //////////////////////
@@ -47,6 +49,11 @@ const tempProduct = fs.readFileSync(
 );
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) =>
+  slugify(el.productName, { lower: true, strict: true })
+);
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
