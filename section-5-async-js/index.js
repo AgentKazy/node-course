eca29789f885dffcd1f85abd2e31a5a7146ecf1c
AgentKazy <kazygaming@outlook.pt>
2024-10-24 22:34:37 +0100

    Step 045

--- section-5-async-js/index.js
+++ section-5-async-js/index.js
@@ -32,11 +32,34 @@ const getDogPic = async () => {
     await writeFilePro('dog-img.txt', res.body.message);
     console.log('Random dog image saved to file.');
   } catch (err) {
-    console.log(err.message);
+    console.log(err);
+    throw err;
   }
+  return '2: READY!';
 };
-getDogPic();
 
+(async () => {
+  try {
+    console.log('1: Will get dog pics.');
+    const x = await getDogPic();
+    console.log(x);
+    console.log('3: Done!');
+  } catch (err) {
+    console.log('ERROR!');
+  }
+})();
+
+/* 
+console.log('1: Will get dog pics.');
+getDogPic()
+  .then((x) => {
+    console.log(x);
+    console.log('3: Done!');
+  })
+  .catch((err) => {
+    console.log('ERROR!');
+  });
+ */
 /*
 readFilePro(`${__dirname}/dog.txt`)
   .then((data) => {
