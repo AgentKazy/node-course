7dd1162a3ffefa70f1140ef905aee8525aa87de9
AgentKazy <kazygaming@outlook.pt>
2024-11-21 17:41:32 +0000

    Step 060

--- section-6-express/app.js
+++ section-6-express/app.js
@@ -1,7 +1,11 @@
 const fs = require('node:fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+
+// 1) MIDDLEWARES
+app.use(morgan('dev'));
 
 app.use(express.json());
 
@@ -15,6 +19,7 @@ app.use((req, res, next) => {
   next();
 });
 
+// 2) ROUTE HANDLERS
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
@@ -109,6 +114,7 @@ const deleteTour = (req, res) => {
 // app.patch('/api/v1/tours/:id', updateTour);
 // app.delete('/api/v1/tours/:id', deleteTour);
 
+// 3) ROUTES
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
 
 app
@@ -117,6 +123,7 @@ app
   .patch(updateTour)
   .delete(deleteTour);
 
+// 4) START SERVER
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
